639828069317c6016ee8492a518bfee2e8f8f41b
Johann Eduardo <johanneduardo18@gmail.com>
2025-08-13 11:58:37 -0300

    Adicionando arquivos de atribuição e ternário.

---
+++ javascript/basico/atribuicao-e-ternario/atribuicao-e-ternario.js
@@ -0,0 +1,23 @@
+var x = 5;
+var y = 10;
+
+
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+
+var podeBeber = (y >= 9) ? 'Pode beber': 'Não pode beber';
+
+console.log (podeBeber)
+
+
+let suaIdade = 18;
+
+if (suaIdade >= 18)
+    console.log('Liberado')
+else
+    console.log('Não pode')
