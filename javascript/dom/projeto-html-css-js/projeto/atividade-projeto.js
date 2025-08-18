1d19770581c38977e42fe96eb9f566b7bdb32bc5
Johann Eduardo <johanneduardo18@gmail.com>
2025-08-18 20:29:57 -0300

    Adicionando arquivos onde lidei com NODE LIST & HTL COLLECTIONS

---
+++ javascript/dom/projeto-html-css-js/projeto/atividade-projeto.js
@@ -0,0 +1,33 @@
+// Retorne no console todas as imagens do site
+
+const img = document.querySelectorAll('img')
+
+console.log(img)
+
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+
+const imgSelecionada = document.querySelectorAll('.animais-lista img')
+
+console.log(imgSelecionada)
+
+
+// Selecione todos os links internos (onde o href começa com #)
+
+const linkInterno = document.querySelectorAll('[href^="#"]')
+
+console.log(linkInterno)
+
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+
+const primeiroAga = document.querySelector('.animais-descricao h2')
+
+console.log(primeiroAga)
+
+// Selecione o último p do site
+
+const paragrafo = document.querySelectorAll ('p')
+
+console.log(paragrafo[paragrafo.length - 1])
+
--- javascript/dom/projeto-html-css-js/projeto/projeto.js
+++ javascript/dom/projeto-html-css-js/projeto/projeto.js
@@ -7,3 +7,8 @@ const linksInternos = document.querySelector('[href^="#"')
 console.log(linksInternos)
 
 // console.log(document.querySelector('ul').innerHTML)
+
+const animaisImg = document.querySelectorAll ('.animais img')
+
+console.log(animaisImg)
+
